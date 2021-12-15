f2515c6d5aaefaaad7f530d68869b9c63821cff1
seb <sebastien.amand@edu.esiee.fr>
2021-12-15 19:29:25 +0100

    Mise en route du service

--- app/src/main/java/com/example/jsuisla/ConfigMessage.java
+++ app/src/main/java/com/example/jsuisla/ConfigMessage.java
@@ -1,17 +1,25 @@
 package com.example.jsuisla;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.os.Messenger;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ConfigMessage extends AppCompatActivity {
 
@@ -24,6 +32,12 @@ public class ConfigMessage extends AppCompatActivity {
 
     Boolean conf_position = false;
     Boolean conf_time = false;
+
+    public static final int PERM_COMPLETED_STORAGE_ACCESS = 1;
+    public static final int WRITE_FILE_REQUEST_CODE = 43;
+
+    public static String id = "test_channel_01";
+
 
 
     @Override
@@ -52,14 +66,60 @@ public class ConfigMessage extends AppCompatActivity {
     public void radioChange(int checkedId){
         if (checkedId == R.id.RB_position){
             conf_position = true;
-        }else if(checkedId == R.id.RB_position){
+        }else if(checkedId == R.id.RB_time){
             conf_time = true;
         }
     }
+    /*
+     * The service will call the handler to send back information.
+     */
+    private Handler handler = new Handler(new Handler.Callback() {
+        @Override
+        public boolean handleMessage(Message msg) {
+            Object path = msg.obj;
+            Toast.makeText(getBaseContext(), path.toString(), Toast.LENGTH_LONG).show();
+            return true;
+        }
+    });
 
     public void ValidConfig(View view){
-        if(conf_position){
-            LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if(conf_time){
+
+            Intent intent = new Intent(this, MyService.class);
+            intent.putExtra("times", 5);
+            Messenger messenger = new Messenger(handler);
+            intent.putExtra("MESSENGER", messenger);
+            createchannel();
+            this.startService(intent);
+
         }
+    }
+    /**
+     * for API 26+ create notification channels
+     */
+    private void createchannel() {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationManager nm = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            NotificationChannel mChannel = new NotificationChannel(id,
+                    "R.string.channel_name",  //name of the channel
+                    NotificationManager.IMPORTANCE_DEFAULT);   //importance level
+            //important level: default is is high on the phone.  high is urgent on the phone.  low is medium, so none is low?
+            // Configure the notification channel.
+            mChannel.setDescription("testChanel");
+            mChannel.enableLights(true);
+            //Sets the notification light color for notifications posted to this channel, if the device supports this feature.
+            mChannel.setLightColor(Color.RED);
+            mChannel.enableVibration(true);
+            mChannel.setShowBadge(true);
+            mChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+            nm.createNotificationChannel(mChannel);
+
+        }
+    }
+    //handle the response, but fully in the fragment.
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[], @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode,permissions,grantResults);
     }
 }
